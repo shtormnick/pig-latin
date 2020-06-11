24d05db3987b53d7f050b95070ee0656490951fa
lorix <foxred324@gmail.com>
2020-06-11 23:00:58 +0300

    refactor code add map

--- main.go
+++ main.go
@@ -12,15 +12,30 @@ func Splitter(s string) []string {
 
 func ToPig(words []string) string {
 	pig := "ay"
+
+	split := map[int]string{
+		1: ",",
+		2: ".",
+		3: "-",
+		4: "?",
+		5: "!",
+		6: " ",
+		7: ":",
+		8: "...",
+	}
+
 	for i, _ := range words {
 		w1 := words[i]
 		s := w1[len(w1)-1:]
-		if s == "," || s == "." || s == "-"  || s == "?" || s == "!" || s == " " || s == ":" || s == "..." {
-			words[i] = w1[1:len(w1)-1] + w1[:1] + pig + s
-		} else {
-			words[i] = w1[1:] + w1[:1] + pig
+		for k := range split {
+			rslt := s == split[k]
+			if rslt {
+				words[i] = w1[1:len(w1)-1] + w1[:1] + pig + s
+			} else {
+				words[i] = w1[1:] + w1[:1] + pig
+			}
 		}
-		  
+
 	}
 	pigwords := strings.Join(words, " ")
 	return pigwords
